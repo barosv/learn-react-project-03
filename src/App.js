5118e5da2daac5fb73f7926b14b72a3747cd2085
Osvaldo <44590317+barosv@users.noreply.github.com>
2020-07-12 16:40:25 -0600

    Complete code

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import Header from './components/Header'
+import Formulario from './components/Formulario'
+import Resumen from './components/Resumen'
+import Resultado from './components/Resultado'
+import styled from '@emotion/styled'
+import Spinner from './components/Spinner';
+
+const Contenedor = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const ContenedorFormulario = styled.div`
+  background-color: #FFFFFF;
+  padding: 3rem;
+`;
+
+function App() {
+
+  const [ resumen, guardarResumen ] = useState({
+    cotizacion: 0,
+    datos: {
+      marca: '',
+      year: '',
+      plan: ''
+    }
+  });
+
+  const { cotizacion, datos } = resumen;
+
+  const [ cargando, guardarCargando ] = useState(false);
+
+  return (
+    <Contenedor>
+      <Header
+        titulo='Cotizador de seguros' 
+      />
+      <ContenedorFormulario>
+        
+        <Formulario 
+          guardarResumen={guardarResumen}
+          guardarCargando={guardarCargando}
+        />
+
+        { cargando ? <Spinner /> : null }
+
+        <Resumen
+          datos={datos}
+        />
+
+        { !cargando ?
+          <Resultado
+            cotizacion={cotizacion}
+          /> :
+          null
+        }
+
+      </ContenedorFormulario>
+    </Contenedor>
+    
+  );
+}
+
+export default App;
